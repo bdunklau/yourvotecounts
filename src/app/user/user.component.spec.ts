aca6a8e404ca4d3241e1da0308b5bd17972e6c4a
bdunklau <bdunklau@yahoo.com>
2019-05-23 12:32:00 -0500

    tests pass!

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -2,16 +2,41 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { UserComponent } from './user.component';
 import { HttpClientModule }    from '@angular/common/http';
+import { AuthService } from '../core/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ActivatedRoute } from '@angular/router';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+
+// a stub/mock
+// FYI  https://github.com/angular/angularfire2/issues/1706#issuecomment-394212606
+const FirestoreStub = {
+    collection: (name: string) => ({
+      doc: (_id: string) => ({
+        valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+        set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+      }),
+    }),
+  };
+
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
 
+  const fakeActivatedRoute = {
+    data: { "mock2": "mock2", subscribe: () => {} },
+    snapshot: { data: { "mock1": "mock" } }
+  } as ActivatedRoute;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ HttpClientModule ],
       declarations: [ UserComponent ],
-      schemas: [ NO_ERRORS_SCHEMA ]
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [AuthService,Location,
+                    { provide: LocationStrategy, useClass: PathLocationStrategy },
+                    {provide: ActivatedRoute, useValue: fakeActivatedRoute},
+                    { provide: AngularFireAuth, useValue: FirestoreStub }]
     })
     .compileComponents();
   }));
