4fe93606c9596e3bb26bb135f82f4ab5d06fab3f
Brent Dunklau <bdunklau@yahoo.com>
2019-05-26 14:36:35 -0500

    put test back in

--- e2e/src/public.e2e-spec.ts
+++ e2e/src/public.e2e-spec.ts
@@ -8,7 +8,7 @@ describe('yourvotecounts - Public page', () => {
     page = new PublicPage();
   });
 
-  xit('when user browses to our app he should see the default “public” screen', () => {
+  it('when user browses to our app he should see the default “public” screen', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('Welcome to yourvotecounts!');
   });
