36a212ffd0ac869c672785db4910d17ae3873177
Brent Dunklau <bdunklau@yahoo.com>
2019-05-26 14:31:02 -0500

    browser.sleep(5000);

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -2,10 +2,14 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo() {
+    browser.sleep(5000);
+    browser.ignoreSynchronization = true;
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
   getTitleText() {
+    browser.sleep(5000);
+    browser.ignoreSynchronization = true;
     return element(by.css('app-root h1')).getText() as Promise<string>;
   }
 }
