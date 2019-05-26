44951e643cc5c3fcb97f71b4914f63ebda403db5
Brent Dunklau <bdunklau@yahoo.com>
2019-05-26 14:33:41 -0500

    browser.sleep(1000);

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -2,13 +2,13 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    browser.sleep(5000);
-    browser.ignoreSynchronization = true;
+    // browser.sleep(5000);
+    // browser.ignoreSynchronization = true;
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
   getTitleText() {
-    browser.sleep(5000);
+    browser.sleep(1000);
     browser.ignoreSynchronization = true;
     return element(by.css('app-root h1')).getText() as Promise<string>;
   }
--- e2e/src/public.po.ts
+++ e2e/src/public.po.ts
@@ -3,13 +3,13 @@ import { browser, by, element } from 'protractor';
 // from  https://blog.cloudboost.io/building-your-first-tests-for-angular5-with-protractor-a48dfc225a75
 export class PublicPage {
   navigateTo() {
-    browser.sleep(5000);
-    browser.ignoreSynchronization = true;
+    // browser.sleep(5000);
+    // browser.ignoreSynchronization = true;
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
   getTitleText() {
-    browser.sleep(5000);
+    browser.sleep(1000);
     browser.ignoreSynchronization = true;
     return element(by.css('app-root h1')).getText() as Promise<string>;
   }
