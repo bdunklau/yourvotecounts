03f71c3bfd497c31e76e3875bd662a05191bd600
Brent Dunklau <bdunklau@yahoo.com>
2019-06-09 18:23:54 -0500

    AdminGuard makes sure only admins can reach certain pages

--- src/app/core/admin.guard.spec.ts
+++ src/app/core/admin.guard.spec.ts
@@ -1,11 +1,43 @@
 import { TestBed, async, inject } from '@angular/core/testing';
-
+import { UserService } from '../user/user.service'
 import { AdminGuard } from './admin.guard';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { HttpClient/*, HttpHeaders, HttpParams, HttpErrorResponse*/ } from '@angular/common/http';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { BehaviorSubject } from 'rxjs';
+import { of } from 'rxjs/observable/of';
+import { Router } from "@angular/router";
+
+
+// a stub/mock
+// FYI  https://github.com/angular/angularfire2/issues/1706#issuecomment-394212606
+const AngularFirestoreStub = {
+    collection: (name: string) => ({
+      doc: (_id: string) => ({
+        valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+        set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+      }),
+    }),
+  };
+
+
+//   // from:   https://atom-morgan.github.io/how-to-test-angular-canactivate-guards/
+// class MockRouter {
+//   navigate(path) {
+//     console.log("MockRouter: path = ", path)
+//   }
+// }
+
 
 describe('AdminGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AdminGuard]
+      providers: [AdminGuard, UserService,
+                  { provide: AngularFirestore, useValue: AngularFirestoreStub },
+                  { provide: AngularFireAuth, useValue: {} },
+                  { provide: Router, useClass: class { navigate = jasmine.createSpy("navigate"); }  },
+                  { provide: HttpClient, useValue: {} }]
     });
   });
 
--- src/app/core/admin.guard.ts
+++ src/app/core/admin.guard.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../user/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
-  canActivate(
+
+  constructor(
+    public userService: UserService,
+    private router: Router
+  ) {}
+
+
+  async canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    state: RouterStateSnapshot): Promise<boolean> {
+
+    try {
+      var user = await this.userService.getCurrentUser()
+
+      console.log("canActivate(): user = ", user)
+      if(user && !user.displayName) {
+        this.router.navigate(['/register']);
+        return false
+      } else if(user) {
+        this.router.navigate(['/user']);
+        return false
+      }
+      else return true;
+    } catch(e) {
+      return true;
+    }
   }
 }
