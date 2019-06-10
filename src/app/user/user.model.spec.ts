ce4c2baa266b1ba308d1aa7ff68cadce113ffc07
Brent Dunklau <bdunklau@yahoo.com>
2019-06-09 23:42:11 -0500

    replaced isAdmin() with hasRole(string)

--- src/app/user/user.model.spec.ts
+++ src/app/user/user.model.spec.ts
@@ -5,23 +5,23 @@ describe('FirebaseUserModel', () => {
   it('should be an Admin 1', () => {
     let u = new FirebaseUserModel()
     u.roles = ["admin"]
-    expect(u.isAdmin()).toBeTruthy();
+    expect(u.hasRole('admin')).toBeTruthy();
   });
 
   it('should be an Admin 2', () => {
     let u = new FirebaseUserModel()
     u.roles = ["role1", "admin"]
-    expect(u.isAdmin()).toBeTruthy();
+    expect(u.hasRole('admin')).toBeTruthy();
   });
 
   it('should not be an Admin 1', () => {
     let u = new FirebaseUserModel()
     u.roles = ["role1"]
-    expect(u.isAdmin()).toBeFalsy();
+    expect(u.hasRole('admin')).toBeFalsy();
   });
 
   it('should not be an Admin 2', () => {
     let u = new FirebaseUserModel()
-    expect(u.isAdmin()).toBeFalsy();
+    expect(u.hasRole('admin')).toBeFalsy();
   });
 })
