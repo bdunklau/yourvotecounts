1572602344ba9e9e72c3abd1eaa52b5eaaf984bc
Brent Dunklau <bdunklau@yahoo.com>
2019-06-09 23:41:25 -0500

    replacing admin.guard with role.guard

--- src/app/core/role.guard.ts
+++ src/app/core/role.guard.ts
@@ -6,32 +6,25 @@ import { UserService } from '../user/user.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AdminGuard implements CanActivate {
+export class RoleGuard implements CanActivate {
 
   constructor(
     public userService: UserService,
     private router: Router
   ) {}
 
-
   async canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Promise<boolean> {
 
-    try {
-      var user = await this.userService.getCurrentUser()
+    var user = await this.userService.getCurrentUser()
 
-      console.log("canActivate(): user = ", user)
-      if(user && !user.displayName) {
-        this.router.navigate(['/register']);
-        return false
-      } else if(user) {
-        this.router.navigate(['/user']);
-        return false
-      }
-      else return true;
-    } catch(e) {
+    if (user.hasRole(next.data.role)) {
       return true;
     }
+
+    // navigate to home page
+    this.router.navigate(['/']);
+    return false;
   }
 }
