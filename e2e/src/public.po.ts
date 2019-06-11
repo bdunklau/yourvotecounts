ff9fb15df44a74a177672adec7e629a5769b74c9
bdunklau <bdunklau@yahoo.com>
2019-06-11 16:53:11 -0500

    work in progress

--- e2e/src/public.po.ts
+++ e2e/src/public.po.ts
@@ -15,13 +15,57 @@ export class PublicPage {
     var el = element(by.name('phoneNumber'))
     el.sendKeys(phone)
 
-    console.log('2222222222222');
-    var iframe = browser.driver.findElements(by.tagName('iframe'));
-    var f = browser.driver.switchTo().frame(iframe);
-    console.log('f = ', f);
-    // f.getWebElement();
 
-    browser.switchTo().defaultContent();
+    // console.log('2222222222222');
+    // element(by.xpath('//app-login')).getText().then(function(text) {
+    //   console.log('text = ', text)
+    //   element(by.xpath('//form/div[2]/div')).getText().then(function(t2) {
+    //     console.log('//form/div[2]/div = ', t2, ' [END]')
+    //   })
+    // })
+    //
+    // browser.sleep(5000);
+
+
+
+    // console.log('2222222222222');
+    // element(by.xpath('//*[@id="firebaseui-auth-container"]/div/form/div[2]/div')).getText().then(function(text) {
+    //   console.log('//*[@id="firebaseui-auth-container"]/div/form/div[2]/div = ', text, ' [END]')
+    // })
+    //
+    // browser.sleep(5000);
+
+
+
+    console.log('2222222222222');
+    var path = '//*[@id="firebaseui-auth-container"]/div/form/div[2]/div/div[3]/div[1]'
+    element(by.xpath(path)).getText().then(function(text) {
+      console.log(path+' = ', text, ' [END]')
+    })
+
+    browser.sleep(5000);
+
+
+
+
+
+
+
+
+
+
+    // var f1 = element(by.xpath("//iframe[1]")).getWebElement();
+    // browser.switchTo().frame(f1);
+    // element(by.tagName('span')).getText().then(function(text) {
+    //   console.log('text = ', text)
+    // })
+    //
+    // browser.sleep(5000);
+
+
+    // browser.switchTo().defaultContent();
+
+
 
     // var driver = browser.driver;
     // console.log('11111111111111')
