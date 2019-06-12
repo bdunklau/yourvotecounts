38b5b4c21b235e5b6c7339241ab38730ad413614
bdunklau <bdunklau@yahoo.com>
2019-06-12 11:30:57 -0500

    Added a logout test

--- e2e/src/public.e2e-spec.ts
+++ e2e/src/public.e2e-spec.ts
@@ -13,10 +13,21 @@ describe('Public page', () => {
 
   it('when user logs in, should be able to get to Register page', () => {
     tokenPage.login(process.env.YOURVOTECOUNTS_NORMAL_PHONE_NUMBER)
-    page.navigateToHome();
+    browser.get(browser.baseUrl);
+    page.clickHome();
     expect(page.getTitleText()).toEqual('home');
-    page.navigateToRegister()
+    page.clickRegister()
     expect(page.getTitleText()).toEqual('Complete Your Account');
+    page.logout();
+  });
+
+  it('should be able to logout', () => {
+    tokenPage.login(process.env.YOURVOTECOUNTS_NORMAL_PHONE_NUMBER);
+    page.clickRegister();
+    expect(page.getTitleText()).toEqual('Complete Your Account');
+    page.logout();
+    page.clickRegister();
+    expect(page.getUrl()).toEqual(browser.baseUrl+'/home');
   });
 
   afterEach(async () => {
