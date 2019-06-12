e7868b573c3077d2e76355ac0ac27003aa353aed
bdunklau <bdunklau@yahoo.com>
2019-06-12 11:32:32 -0500

    added logout() method and changed navigateToXxxxx() to clickXxxx() because browser.get(url) sometimes behaves differently than clicking links.  One one occassion, noticed the url when to localhost:4200/ and stayed there rather than redirecting to /home as I would expect

--- e2e/src/public.po.ts
+++ e2e/src/public.po.ts
@@ -3,20 +3,32 @@ import * as protractor from 'protractor'
 
 // from  https://blog.cloudboost.io/building-your-first-tests-for-angular5-with-protractor-a48dfc225a75
 export class PublicPage {
-  navigateToHome() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  clickHome() {
+    element(by.id('home_menu_item')).click()
+    browser.sleep(100); // worked at 500
   }
 
-  navigateToRegister() {
-    var ret = browser.get(browser.baseUrl+'/register') as Promise<any>;
-    browser.sleep(1000);
-    return ret;
+  clickRegister() {
+    element(by.id('register_menu_item')).click()
+    browser.sleep(1000); // lower values don't always work
+  }
+
+  clickUser() {
+    element(by.id('user_menu_item')).click()
   }
 
   getTitleText() {
     browser.sleep(100);
     browser.ignoreSynchronization = true;
     return element(by.css('app-root h1')).getText() as Promise<string>;
+  }
+
+  logout() {
+    element(by.xpath('//*[@id="navbarSupportedContent"]/ul/li[3]/a')).click()
+  }
+
+  getUrl() {
+    return browser.getCurrentUrl();
   }
 
 }
