0ef428ea88b617fd5f3e657f6fb7150f53e9f12c
Brent Dunklau <bdunklau@yahoo.com>
2019-06-11 23:05:30 -0500

    ng test passes again.  Had template parse errors in token.component.spec.ts because imports were missing

--- src/app/token/token.component.spec.ts
+++ src/app/token/token.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { TokenComponent } from './token.component';
 
 describe('TokenComponent', () => {
@@ -8,6 +8,7 @@ describe('TokenComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ ReactiveFormsModule, FormsModule ],
       declarations: [ TokenComponent ]
     })
     .compileComponents();
