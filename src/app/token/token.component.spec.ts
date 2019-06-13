4500d3944af727411b495047b674dbd01e64918f
bdunklau <bdunklau@yahoo.com>
2019-06-13 09:23:08 -0500

    Notice how we're importing CommonServiceModuleStub and AngularFireAuthStub and we're using AngularFireAuthStub as a provider in the providers array.  This is what we need to do with AngularFirestoreStub, instead of duplicating the definition of AngularFirestoreStub all over the place.

--- src/app/token/token.component.spec.ts
+++ src/app/token/token.component.spec.ts
@@ -1,6 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { /*Component, DebugElement,*/ NO_ERRORS_SCHEMA } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { TokenComponent } from './token.component';
+import { UserService } from '../user/user.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { HttpClient/*, HttpHeaders, HttpParams, HttpErrorResponse*/ } from '@angular/common/http';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {CommonServiceModuleStub, AngularFireAuthStub} from '../core/common.module'
+
+// a stub/mock
+// FYI  https://github.com/angular/angularfire2/issues/1706#issuecomment-394212606
+const AngularFirestoreStub = {
+    collection: (name: string) => ({
+      doc: (_id: string) => ({
+        valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+        set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+      }),
+    }),
+  };
+
 
 describe('TokenComponent', () => {
   let component: TokenComponent;
@@ -8,7 +26,13 @@ describe('TokenComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ ReactiveFormsModule, FormsModule ],
+      imports: [ ReactiveFormsModule, FormsModule, CommonServiceModuleStub ],
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [ UserService,
+                  { provide: AngularFirestore, useValue: AngularFirestoreStub },
+                  { provide: AngularFireAuth, useValue: AngularFireAuthStub },
+                  { provide: HttpClient, useValue: {} }
+                 ],
       declarations: [ TokenComponent ]
     })
     .compileComponents();
