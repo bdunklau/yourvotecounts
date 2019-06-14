1972e7c06c67e0a3ac5e8a306865a9eff793c316
bdunklau <bdunklau@yahoo.com>
2019-06-14 10:15:21 -0500

    got test to pass

--- src/app/my-account/my-account.component.spec.ts
+++ src/app/my-account/my-account.component.spec.ts
@@ -1,14 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule/*, ReactiveFormsModule, FormBuilder*/ } from '@angular/forms';
 import { MyAccountComponent } from './my-account.component';
+import { UserService } from '../user/user.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { HttpClient/*, HttpHeaders, HttpParams, HttpErrorResponse*/ } from '@angular/common/http';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 describe('MyAccountComponent', () => {
   let component: MyAccountComponent;
   let fixture: ComponentFixture<MyAccountComponent>;
 
+  const AngularFirestoreStub = {
+      collection: (name: string, f: (ref:any) => {}) => ({
+        doc: (_id: string) => ({
+          valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+          set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+        }),
+        valueChanges: () => of([{id: '1', event: 'event1', date: {toDate: () => new Date()}}, // 2 mock LogEntry's
+                                {id: '2', event: 'event2', date: {toDate: () => new Date()}}]) // THIS IS NOT WHAT WE WANT TO MOCK HERE.
+                                // This was taken from log.component.spec.ts
+      }),
+    };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MyAccountComponent ]
+      imports: [ FormsModule ],
+      declarations: [ MyAccountComponent ],
+      providers: [ UserService,
+                  { provide: HttpClient, useValue: {} },
+                  { provide: AngularFireAuth, useValue: {} },
+                  { provide: AngularFirestore, useValue: AngularFirestoreStub },
+                ]
     })
     .compileComponents();
   }));
