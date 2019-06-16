f34a9a8a0f71b483e92a840e2ece2f95f9e7bb24
Brent Dunklau <bdunklau@yahoo.com>
2019-06-15 20:03:53 -0500

    display doc id of log entries.  May not want to keep this.  snapshotChanges() is what gives you the doc id metadata.  If you don't want this metadata, use valueChanges()

--- src/app/log/log.component.ts
+++ src/app/log/log.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { LogEntry } from './logentry'
 import { Observable } from 'rxjs/Observable'
+import { map } from 'rxjs/operators';
+import * as firebase from 'firebase/app';
+import { LogService } from '../log/log.service';
+
 
 @Component({
   selector: 'app-log',
@@ -13,16 +17,29 @@ export class LogComponent implements OnInit {
   logRef: AngularFirestoreCollection<LogEntry>;
   log: Observable<LogEntry[]>;
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore,
+              private logService: LogService) { }
 
   // helpful for getting queries working:  https://www.youtube.com/watch?v=SGQGFO_zkx4&t=409s
   ngOnInit() {
-    this.logRef = this.afs.collection('log', ref => ref.orderBy('date_ms').limit(10))
-    this.log = this.logRef.valueChanges()
+    this.logRef = this.afs.collection('log', ref => ref.orderBy('date_ms', 'desc').limit(50));
+    this.log = this.logRef.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as LogEntry;
+        const id = a.payload.doc.id;
+        var thedata = { id, ...data };
+        console.log('LogComponent:ngOnInit() thedata: ', thedata);
+        return thedata;
+      }))
+    );
   }
-  
+
+  logout() {
+    this.logService.logout(firebase.auth().currentUser);
+  }
+
   ngOnDestroy() {
-    // this.logRef.unsubscribe(); // we never called subscribe
+    // we never called subscribe
   }
 
 }
