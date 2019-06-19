a372311e8f38eaed8a673f819ff11a825914b104
Brent Dunklau <bdunklau@yahoo.com>
2019-06-19 01:02:46 -0500

    alphabetize methods

--- e2e/src/admin.po.ts
+++ e2e/src/admin.po.ts
@@ -5,12 +5,13 @@ import { BasePage } from './base.po';
 // from  https://blog.cloudboost.io/building-your-first-tests-for-angular5-with-protractor-a48dfc225a75
 export class AdminPage extends BasePage {
 
-  setLevel(level) {
-    var levelDropdown = this.getElement(by.id('levelDropdown'))
-  }
-
   getLogEntries(level): ElementArrayFinder {
     return this.getElementsByText(level);
+  }
+
+  setLevel(level) {
+    this.getElement(by.id('levelDropdown')).click();
+    this.getElement(by.id(level+'Level')).click();
   }
 
 }
