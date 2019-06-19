f7bd30d4b3abef8d39b7bc203b3d265be648185f
Brent Dunklau <bdunklau@yahoo.com>
2019-06-19 08:44:32 -0500

    getLevel(), getLogEntries()

--- e2e/src/admin.po.ts
+++ e2e/src/admin.po.ts
@@ -5,7 +5,12 @@ import { BasePage } from './base.po';
 // from  https://blog.cloudboost.io/building-your-first-tests-for-angular5-with-protractor-a48dfc225a75
 export class AdminPage extends BasePage {
 
+  getLevel() {
+    return this.getElement(by.id('levelDropdown')).getText();
+  }
+
   getLogEntries(level): ElementArrayFinder {
+    browser.sleep(3000);
     return this.getElementsByText(level);
   }
 
