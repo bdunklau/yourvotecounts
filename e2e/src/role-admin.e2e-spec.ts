d36128608e2c0c54160f8389209d13ff9d443e35
bdunklau <bdunklau@yahoo.com>
2019-06-19 09:07:09 -0500

    test passes: should be able to view logs by level

--- e2e/src/role-admin.e2e-spec.ts
+++ e2e/src/role-admin.e2e-spec.ts
@@ -3,7 +3,7 @@ import { browser, logging, element, by } from 'protractor';
 import { TestSupport } from './test-support.po';
 import { AdminPage } from './admin.po';
 
-fdescribe('Admins', () => {
+describe('Admins', () => {
   // let page: PublicPage;
   let page: MainPage;
   let testSupport: TestSupport;
@@ -23,7 +23,7 @@ fdescribe('Admins', () => {
     page.clickLogout()
   });
 
-  fit('should be able to view logs by level', async () => {
+  it('should be able to view logs by level', async () => {
     // db setup - have to log error, info and debug entries so we have something
     // to test
     testSupport.createLogs();
@@ -31,38 +31,38 @@ fdescribe('Admins', () => {
     testSupport.login(process.env.YOURVOTECOUNTS_ADMIN_PHONE_NUMBER);
     page.clickLog();
     var actualLevel;
-    // adminPage.setLevel('error');
-    // actualLevel = await adminPage.getLevel();
-    // expect(actualLevel === 'error').toBeTruthy('expected level to be error, not '+actualLevel);
-    // adminPage.getLogEntries('error').then(function(numbers){
-    //   console.log('In error log, found '+numbers.length+' error elements')
-    //   expect(numbers.length >= 2 ).toBeTruthy('expected at least 2 instances of "error" on error log page but got '+numbers.length);
-    // });
-    // adminPage.getLogEntries('info').then(function(numbers){
-    //   console.log('In error log, found '+numbers.length+' info elements');
-    //   expect(numbers.length === 1).toBeTruthy('expected 1 instance of "info" on the error log but got '+numbers.length);
-    // });
-    // adminPage.getLogEntries('debug').then(function(numbers){
-    //   console.log('In error log, found '+numbers.length+' debug elements');
-    //   expect(numbers.length === 1).toBeTruthy('expected 1 instance of "debug" on the error log but got '+numbers.length);
-    // });
+    adminPage.setLevel('error');
+    actualLevel = await adminPage.getLevel();
+    expect(actualLevel === 'error').toBeTruthy('expected level to be error, not '+actualLevel);
+    adminPage.getLogEntries('error').then(function(numbers){
+      console.log('In error log, found '+numbers.length+' error elements')
+      expect(numbers.length >= 2 ).toBeTruthy('expected at least 2 instances of "error" on error log page but got '+numbers.length);
+    });
+    adminPage.getLogEntries('info').then(function(numbers){
+      console.log('In error log, found '+numbers.length+' info elements');
+      expect(numbers.length === 1).toBeTruthy('expected 1 instance of "info" on the error log but got '+numbers.length);
+    });
+    adminPage.getLogEntries('debug').then(function(numbers){
+      console.log('In error log, found '+numbers.length+' debug elements');
+      expect(numbers.length === 1).toBeTruthy('expected 1 instance of "debug" on the error log but got '+numbers.length);
+    });
 
 
-    // adminPage.setLevel('info');
-    // actualLevel = await adminPage.getLevel();
-    // expect(actualLevel === 'info').toBeTruthy('expected level to be info, not '+actualLevel);
-    // adminPage.getLogEntries('error').then(function(numbers){
-    //   console.log('In info log, found '+numbers.length+' error elements');
-    //   expect(numbers.length >= 2 ).toBeTruthy('expected at least 2 instances of "error" on info log page but got '+numbers.length);
-    // });
-    // adminPage.getLogEntries('info').then(function(numbers){
-    //   console.log('In info log, found '+numbers.length+' info elements');
-    //   expect(numbers.length >= 2 ).toBeTruthy('expected at least 2 instances of "info" on info log page but got '+numbers.length);
-    // });
-    // adminPage.getLogEntries('debug').then(function(numbers){
-    //   console.log('In info log, found '+numbers.length+' debug elements');
-    //   expect(numbers.length === 1).toBeTruthy('expected 1 instance of "debug" on the info log but got '+numbers.length);
-    // });
+    adminPage.setLevel('info');
+    actualLevel = await adminPage.getLevel();
+    expect(actualLevel === 'info').toBeTruthy('expected level to be info, not '+actualLevel);
+    adminPage.getLogEntries('error').then(function(numbers){
+      console.log('In info log, found '+numbers.length+' error elements');
+      expect(numbers.length >= 2 ).toBeTruthy('expected at least 2 instances of "error" on info log page but got '+numbers.length);
+    });
+    adminPage.getLogEntries('info').then(function(numbers){
+      console.log('In info log, found '+numbers.length+' info elements');
+      expect(numbers.length >= 2 ).toBeTruthy('expected at least 2 instances of "info" on info log page but got '+numbers.length);
+    });
+    adminPage.getLogEntries('debug').then(function(numbers){
+      console.log('In info log, found '+numbers.length+' debug elements');
+      expect(numbers.length === 1).toBeTruthy('expected 1 instance of "debug" on the info log but got '+numbers.length);
+    });
 
 
     adminPage.setLevel('debug');
