511a521d851e4b2755d98f718674e3fc2ec22e9e
bdunklau <bdunklau@yahoo.com>
2019-06-21 16:59:44 -0500

    adding search by date to the Log screen

--- src/app/util/date-chooser/date-chooser.component.ts
+++ src/app/util/date-chooser/date-chooser.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter/*, Input*/, Output, Injectable } from '@angular/core';
+import {NgbDate, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-date-chooser',
   templateUrl: './date-chooser.component.html',
-  styleUrls: ['./date-chooser.component.css']
+  styleUrls: ['./date-chooser.component.css'],
 })
-export class DateChooserComponent implements OnInit {
+export class DateChooserComponent {
 
-  constructor() { }
+  model;
+  @Output() enteredDate = new EventEmitter<string>();
 
-  ngOnInit() {
+  // $event is an NgbDate with day, month, year attributes
+  select($event) {
+    var d1 = new Date($event.year, $event.month - 1, $event.day);
+    var d2 = new Date($event.year, $event.month - 1, $event.day + 1);
+        console.log('select: ', d2);
+
+    // this.model = $event.item
+    // this.enteredDate.emit($event.item.displayName);
+  }
+
+  dateChanged($event) {
+    var d1 = new Date($event.year, $event.month - 1, $event.day);
+    var d2 = new Date($event.year, $event.month - 1, $event.day + 1);
+           console.log('dateChanged: ', d2);
+    // this.model = $event.item
+    // this.enteredDate.emit($event.item.displayName);
   }
 
 }
