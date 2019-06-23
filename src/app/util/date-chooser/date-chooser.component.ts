bf94b36ab32d2d691c0b5aa2167a14bd56df53e5
Brent Dunklau <bdunklau@yahoo.com>
2019-06-23 01:35:51 -0500

    handling $event object

--- src/app/util/date-chooser/date-chooser.component.ts
+++ src/app/util/date-chooser/date-chooser.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter/*, Input*/, Output, Injectable } from '@angular/core';
 import {NgbDate, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-date-chooser',
@@ -13,18 +14,18 @@ export class DateChooserComponent {
 
   // $event is an NgbDate with day, month, year attributes
   select($event) {
-    this.emitDate($event);
-  }
-
-  dateChanged($event) {
-    this.emitDate($event);
-  }
-
-  emitDate($event) {
     var d1 = new Date($event.year, $event.month - 1, $event.day);
     var d2 = new Date($event.year, $event.month - 1, $event.day + 1);
     this.dateSelected.emit({date1: d1.getTime(), date2: d2.getTime()});
-      console.log('emitDate: d1 = ', d1);
+    // console.log('emitDate: d1 = ', d1);
+  }
+
+  // fires when you manually type in a date
+  dateChanged($event) {
+    var d1 = moment($event, 'MM/DD/YYYY').toDate();
+    var d2 = moment($event, 'MM/DD/YYYY').add(1, 'days').toDate();
+    this.dateSelected.emit({date1: d1.getTime(), date2: d2.getTime()});
+    // console.log('emitDate: d1 = ', d1);
   }
 
 }
