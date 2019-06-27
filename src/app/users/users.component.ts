4cf38700525984ccbfbcf601b34c4d37ee86207f
Brent Dunklau <bdunklau@yahoo.com>
2019-06-26 23:04:22 -0500

    greatly simplified the component because we are passing the User object around instead of just the name and phone number

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { FirebaseUserModel } from '../user/user.model'
-import { Observable, Subject, Subscription } from 'rxjs/Observable'
-import { UserService } from '../user/user.service'
+import { FirebaseUserModel } from '../user/user.model';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-users',
@@ -11,46 +9,22 @@ import { UserService } from '../user/user.service'
 })
 export class UsersComponent implements OnInit {
 
-  subject = new Subject<any>();
-  nameVal: string;
-  // respbody: String;
-  // headers: String[];
-  // ref: AngularFirestoreCollection<FirebaseUserModel>;
-  // list: Observable<FirebaseUserModel[]>;
-  subscription: Subscription;
+  user: FirebaseUserModel;
 
-  constructor(/*private afs: AngularFirestore,*/
-              public us: UserService) { }
+  constructor(public us: UserService) { }
 
   ngOnInit() {
-    // console.log("this.afs = ", this.afs)
-    // console.log("this.afs.collection = ", this.afs.collection)
-    // this.ref = this.afs.collection('user', ref => ref.orderBy('date_ms'))
-    // this.list = this.ref.valueChanges()
-
-
-    // aka Dynamic Query
-    this.subscription = this.subject.pipe(/*
-      need take(1) or something like that
-      Only want a single query - nothing observable
-    */).subscribe((something) => {
-      let xx:LogEntry[] = something as LogEntry[];
-      //console.log('xx = ', xx)
-      this.log = xx;
-    });
   }
 
-  // deleteUser(uid: String) {
-  //   this.us.deleteUser(uid)
-  //   .subscribe(resp => {
-  //     console.log("resp: ", resp) // json formatted string
-  //   });
-  // }
+  ngOnDestroy() {
+  }
 
-  onNameEntered(nameVal: string) {
-    // console.log('onNameEntered: nameVal = ', nameVal);
-    this.nameVal = nameVal;
-    this.subject.next({nameVal: nameVal});
+  onUserSelectedByName(user: FirebaseUserModel) {
+    this.user = user;
+  }
+
+  onUserSelectedByPhone(user: FirebaseUserModel) {
+    this.user = user;
   }
 
 }
