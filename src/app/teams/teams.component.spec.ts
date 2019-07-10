9865db244c106bb321f280a5d2ac4971b50cf23b
bdunklau <bdunklau@yahoo.com>
2019-07-10 17:00:54 -0500

    working on getting jasmine unit tests to pass

--- src/app/teams/teams.component.spec.ts
+++ src/app/teams/teams.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { /*Component, DebugElement,*/ NO_ERRORS_SCHEMA } from '@angular/core';
 import { TeamsComponent } from './teams.component';
 
 describe('TeamsComponent', () => {
@@ -8,7 +8,8 @@ describe('TeamsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TeamsComponent ]
+      declarations: [ TeamsComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
   }));
