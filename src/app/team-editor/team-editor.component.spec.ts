80b66459b35982ecb87c51eae554061333cec039
bdunklau <bdunklau@yahoo.com>
2019-07-17 13:23:03 -0500

    jasmine unit tests pass again

--- src/app/team-editor/team-editor.component.spec.ts
+++ src/app/team-editor/team-editor.component.spec.ts
@@ -8,6 +8,10 @@ import { UserService } from '../user/user.service';
 import { HttpClient/*, HttpHeaders, HttpParams, HttpErrorResponse*/ } from '@angular/common/http';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { BehaviorSubject } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Router } from "@angular/router";
 
 // a stub/mock
 // FYI  https://github.com/angular/angularfire2/issues/1706#issuecomment-394212606
@@ -28,11 +32,19 @@ describe('TeamEditorComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TeamEditorComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ],
       imports: [ FormsModule, NgbModule],
       providers: [UserService,
+                  { provide: Router, useClass: class { navigate = jasmine.createSpy("navigate"); } },
                   {provide: HttpClient, useValue: {} },
                   { provide: AngularFireAuth, useValue: {} },
-                  {provide: AngularFirestore, useValue: FirestoreStub}
+                  {provide: AngularFirestore, useValue: FirestoreStub},
+                  {provide: ActivatedRoute,
+                    useValue: {
+                      params: Observable.from([{id: 1}]),
+                      data: { "mock2": "mock2", subscribe: () => {} }
+                    },
+                  },
                 ]
     })
     .compileComponents();
--- src/app/team-viewer/team-viewer.component.spec.ts
+++ src/app/team-viewer/team-viewer.component.spec.ts
@@ -1,14 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TeamViewerComponent } from './team-viewer.component';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 describe('TeamViewerComponent', () => {
   let component: TeamViewerComponent;
   let fixture: ComponentFixture<TeamViewerComponent>;
 
+    const AngularFirestoreStub = {
+        collection: (name: string, f: (ref:any) => {}) => ({
+          doc: (_id: string) => ({
+            valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+            set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+          }),
+          valueChanges: () => of([{id: '1', event: 'event1', date: {toDate: () => new Date()}}, // 2 mock LogEntry's
+                                  {id: '2', event: 'event2', date: {toDate: () => new Date()}}]) // THIS IS NOT WHAT WE WANT TO MOCK HERE.
+                                  // This was taken from log.component.spec.ts
+        }),
+      };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TeamViewerComponent ]
+      declarations: [ TeamViewerComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [ {provide: ActivatedRoute,
+                    useValue: {
+                      params: Observable.from([{id: 1}]),
+                      data: { "mock2": "mock2", subscribe: () => {} }
+                    },
+                  },
+                  { provide: AngularFirestore, useValue: AngularFirestoreStub },
+                ]
     })
     .compileComponents();
   }));
