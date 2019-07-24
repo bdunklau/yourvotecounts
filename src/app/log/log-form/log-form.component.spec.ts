69b728ea488050082bffe6c1a6071fc599fde081
bdunklau <bdunklau@yahoo.com>
2019-07-24 14:43:05 -0500

    forgot import

--- src/app/log/log-form/log-form.component.spec.ts
+++ src/app/log/log-form/log-form.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {FormsModule} from '@angular/forms';
 import { LogFormComponent } from './log-form.component';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 
 
 describe('LogFormComponent', () => {
