94203ecce0b11099452a63bb6056e5de178b5a00
Brent Dunklau <bdunklau@yahoo.com>
2019-07-31 20:03:32 -0500

    jasmine tests should pass now

--- src/app/pagination/pagination.service.spec.ts
+++ src/app/pagination/pagination.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { PaginationService } from './pagination.service';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { BehaviorSubject, of } from 'rxjs';
 
 
 describe('PaginationService', () => {
