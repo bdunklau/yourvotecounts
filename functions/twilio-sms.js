c8648962c5afd25ccb95b8c871356f01478e2065
Brent Dunklau <bdunklau@yahoo.com>
2019-08-13 07:40:59 -0500

    working on sms

--- functions/twilio-sms.js
+++ functions/twilio-sms.js
@@ -1,6 +1,7 @@
 
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
+const twilio = require('twilio')
 
 // can only call this once globally and we already do that in index.js
 //admin.initializeApp(functions.config().firebase);
@@ -11,29 +12,40 @@ var db = admin.firestore();
 
 
 exports.sendSms = functions.https.onRequest(async (req, res) => {
-  let authKeyValid = await twilioAuthKeyValidated(req.query.auth_key);
-  if(!authKeyValid)
-    return res.status(200).send('<h3>error</h3><br/><h2>not authorized (code 4)</h2>')
+  let settings = await getSettings(req.query.auth_key);
+  if(!settings.valid)
+    return res.status(200).send('<h3>error</h3><br/><h2>not authorized</h2>')
 
-  var resp = 'OK - good key';
-  res.status(200).send(resp);
+  // return res.status(200).send('account sid: '+settings.twilio_account_sid);
+
+  // require the Twilio module and create a REST client
+  const client = twilio(settings.twilio_account_sid, settings.twilio_auth_key);
+  return client.messages
+    .create({
+      to: '+12146325613',//+req.body.to,
+      from: '+18174094501',//+req.body.from,
+      body: 'test message', //req.body.message,
+      mediaUrl: 'https://www.yahoo.com',
+    })
+    .then((message) => {
+      return res.status(200).send('message sent: '+message.sid)
+    });
+
+
 })
 
 
-var twilioAuthKeyValidated = function(auth_key) {
+var getSettings = function(auth_key) {
 
   return new Promise((resolve, reject) => {
     if(!auth_key) {
-      resolve(false);
+      resolve('no key');
     }
 
-    return db.collection('config').doc('settings').get().then(snap => {
-      // auth_key not in the database?  quit early
-      if(snap.size === 0) {
-        resolve(false);
-        return;
-      }
-      resolve(snap.auth_key === auth_key)
+    return db.collection('config').doc('settings').get().then(doc => {
+      var settings = doc.data();
+      settings.valid = doc.data().twilio_auth_key === auth_key;
+      resolve(settings);
       return;
     })
 
