11120a65a3551776867659fb825252357f47b6d6
Brent Dunklau <brentdunklau@csitx.com>
2020-08-14 10:26:48 -0500

    phone number validation on the invitation form - pretty cool

--- src/app/invitation/invitation-form/invitation-form.component.ts
+++ src/app/invitation/invitation-form/invitation-form.component.ts
@@ -3,6 +3,14 @@ import { Invitation } from '../invitation.model';
 import { NgForm, /*FormControl, FormGroup*/ } from '@angular/forms';
 import { Router } from "@angular/router";
 import { InvitationService } from '../invitation.service';
+import {
+  AbstractControl ,
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
+import { isInteger } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 
 @Component({
@@ -12,24 +20,53 @@ import { InvitationService } from '../invitation.service';
 })
 export class InvitationFormComponent implements OnInit {
 
+  invitationForm: FormGroup;
   invitation: Invitation;
-  nameValue: string;
-  phoneValue: string;
+  //nameValue: string;
+  //phoneValue: string;
+  phonePattern = "^((\\+91-?)|0)?[0-9]{10}$";
 
   constructor(
+    private fb: FormBuilder,
     private invitationService: InvitationService,
     private router: Router,) { }
 
   ngOnInit(): void {
     this.invitation = new Invitation();
+    this.createForm();
+  }
+  
+  get name() {
+    return this.invitationForm.get("name");
+  }
+
+  get phone() {
+    return this.invitationForm.get("phone");
+  }
+
+  createForm() {
+    this.invitationForm = this.fb.group(
+      {
+        name: ["", [Validators.required]],
+        phone: ["", { validators: [Validators.required, this.ValidatePhone], updateOn: "blur" }],
+      },
+      // { updateOn: "blur" } <-- will apply to entire form
+    );
+  }
+  
+  ValidatePhone(control: AbstractControl): {[key: string]: any} | null  {
+    if (control.value && (control.value.length != 10 || isNaN(control.value)) ) {
+      return { 'phoneNumberInvalid': true };
+    }
+    return null;
   }
   
 
-  onSubmit(form: NgForm) {
+  onSubmit(/*form: NgForm*/) {
     var invitationId = null;
     //if(this.teamIdValue) this.team.id = this.teamIdValue;
-    this.invitation.displayName = this.nameValue.trim();
-    this.invitation.phoneNumber = this.phoneValue.trim();
+    this.invitation.displayName = this.invitationForm.get("name").value.trim();
+    this.invitation.phoneNumber = this.invitationForm.get("phone").value.trim();
     if(!this.invitation.phoneNumber.startsWith('+1'))
       this.invitation.phoneNumber = '+1'+this.invitation.phoneNumber;
     
@@ -45,12 +82,12 @@ export class InvitationFormComponent implements OnInit {
     }
     *****/
     //this.router.navigate(['/teams', teamId]);
-    form.reset();
+    this.invitationForm.reset();
   }
 
-  cancel(form: NgForm) {
+  cancel(/*form: NgForm*/) {
     // this.editing.emit(false);
-    form.reset();
+    this.invitationForm.reset();
   }
 
 }
