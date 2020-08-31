f8a2c8adae58a1ffa62bd0319eaf0df6234482c2
Brent Dunklau <brentdunklau@csitx.com>
2020-08-31 13:41:52 -0500

    added comment about declaring fields - that they don't have to be explicitly declared

--- src/app/room/room-obj.model.ts
+++ src/app/room/room-obj.model.ts
@@ -1,5 +1,12 @@
 export class RoomObj {
     
+/**
+ * We don't really have to specify every attribute
+ * We can just do stuff like  roomObj['whatever_attribute'] instead of declaring a whatever_attribute field
+ * 
+ * If we want to do something like  roomDoc.data() as RoomObj, THEN WE DO HAVE TO DEFINE FIELDS EXPLICITLY
+ */
+
     RoomSid: string
     created_ms: number
     guests: {guestName: string, guestPhone: string, joined_ms?: number}[]
@@ -7,8 +14,10 @@ export class RoomObj {
     hostName: string
     hostPhone: string
     host_joined_ms?: number
+    host_left_ms?: number
     invitationId: string
     call_ended_ms?: number
+    //mark_time: {"start_recording_ms": number, "start_recording": string, "duration": string}[]
 
     constructor() {
 
