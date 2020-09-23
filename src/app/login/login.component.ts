9ca87652073f8d33f7a65a57840b89dd9f0ca9ec
Brent Dunklau <brentdunklau@csitx.com>
2020-09-22 23:47:42 -0500

    got angular universal working with ssr (server side rendering) including login which was a real trick because the firebaseui library uses the window object which doesn't exist in node

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 // import * as firebaseui from 'firebaseui'
 // import { firebase} from 'firebase/app'
-import {firebase, firebaseui} from 'firebaseui-angular'
+//import {firebase, firebaseui} from 'firebaseui-angular'
 import { BehaviorSubject, of } from 'rxjs';
 import { LogService } from '../log/log.service'
 import { UserService } from '../user/user.service'
 import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -20,36 +21,65 @@ export class LoginComponent implements OnInit {
   constructor(private afAuth: AngularFireAuth,
               private log: LogService,
               private router: Router,
+              @Inject(PLATFORM_ID) private platformId,
               private userService: UserService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     console.log("login.component.ts: ngOnInit()")
 
-    // see:  https://www.youtube.com/watch?v=vAX7PyhbU6s
-    const uiConfig = {
-      signInOptions: [
-        firebase.auth.PhoneAuthProvider.PROVIDER_ID
-      ],
-      callbacks: {
-        signInSuccessWithAuthResult: this.onLoginSuccessful.bind(this)
-      },
+    if(isPlatformBrowser(this.platformId)) {
+        let firebase = await import('firebase')
+        let firebaseui = await import('firebaseui')
+        console.log('firebaseui = ', firebaseui)
+
+
+
+        
+        let onLoginSuccessful = function() {
+          console.log("onLoginSuccessful()");
+          var user = firebase.auth().currentUser;
+          if(user){
+            // this.log.i('login');
+            // this.userService.setFirebaseUser(user);
+            this.userService.signIn(this.log, user);
+            this.router.navigate(['/home'])
+          }
+        }
+
+        console.log('ah ha!!')
+
+
+        // see:  https://www.youtube.com/watch?v=vAX7PyhbU6s
+        const uiConfig = {
+          signInOptions: [
+              firebase.auth.PhoneAuthProvider.PROVIDER_ID
+          ],
+          callbacks: {
+              signInSuccessWithAuthResult: /*this.*/onLoginSuccessful.bind(this)
+          },
+        }
+
+        // starts the firebase ui library
+        this.ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(this.afAuth.auth);
+        this.ui.start('#firebaseui-auth-container', uiConfig);
+      
+
+
     }
 
-    // starts the firebase ui library
-    this.ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(this.afAuth.auth);
-    this.ui.start('#firebaseui-auth-container', uiConfig);
   }
 
-  onLoginSuccessful() {
-    console.log("onLoginSuccessful()");
-    var user = firebase.auth().currentUser;
-    if(user){
-      // this.log.i('login');
-      // this.userService.setFirebaseUser(user);
-      this.userService.signIn(this.log, user);
-      this.router.navigate(['/home'])
-    }
-  }
+
+  // onLoginSuccessful() {
+  //   console.log("onLoginSuccessful()");
+  //   var user = firebase.auth().currentUser;
+  //   if(user){
+  //     // this.log.i('login');
+  //     // this.userService.setFirebaseUser(user);
+  //     this.userService.signIn(this.log, user);
+  //     this.router.navigate(['/home'])
+  //   }
+  // }
 
 
 }
