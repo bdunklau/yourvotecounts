0bd1177a76303b64e3c5db214abfceee0445ccac
Brent Dunklau <brentdunklau@csitx.com>
2020-10-07 23:16:19 -0500

    just a note about resolvers.  And left as a comment the code to get the Settings doc

--- src/app/settings/settings.guard.ts
+++ src/app/settings/settings.guard.ts
@@ -23,12 +23,17 @@ export class SettingsGuard implements CanActivate {
       if(isBrowser) {
 
           // should this be in a resolver?
-          // are resolvers called before or after guards?  I need a settings resolver to be called before all the
+          // GUARDS ARE CALLED BEFORE RESOLVERS  ***********************
+          // I need a settings resolver to be called before all the
           // guards in search-officials.guard
           // Since I'm not sure when resolvers are called, I do the "resolving" here in this guard
           let keys = await this.settingsService.getKeysDoc()
           this.settingsService.keys = keys
-          //console.log('SettingsGuard:  this.settingsService.keys = ', this.settingsService.keys)
+
+          // TODO should probably be in a resolver instead...
+          // let settings = await this.settingsService.getSettingsDoc()
+          // this.settingsService.settings = settings
+
           return true
       }
       else return true
