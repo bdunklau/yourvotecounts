dc2d5876516b9259b46165793d7850f91db88f64
Brent Dunklau <brentdunklau@csitx.com>
2020-11-07 02:22:33 -0600

    make test pass.  Notice the file we added to karma.conf.js and how we inject a mock SettingsService object in search-officials.component.spec.ts

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -12,6 +12,9 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    files: [
+      'https://apis.google.com/js/client.js?onload=init'
+    ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
