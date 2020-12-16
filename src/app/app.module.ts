74d2ee3d224e202f2bed7e238332d6ea0589b540
Brent Dunklau <brentdunklau@csitx.com>
2020-12-15 21:52:59 -0600

    globalThis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -75,7 +75,7 @@ import { SettingsService } from './settings/settings.service';
 import { isPlatformBrowser } from '@angular/common';
 import { VideoListComponent } from './admin/video/video-list/video-list.component';
 
-var globalThis = require('globalthis/polyfill')();
+import globalThis  from 'globalthis/polyfill'
 
 
 const ORIGINAL_NOT_USED_firebaseUiAuthConfig: firebaseui.auth.Config = {
