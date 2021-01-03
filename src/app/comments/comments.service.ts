cc5ac3c3f6140a59d95fd09856086078896503fb
Brent Dunklau <brentdunklau@csitx.com>
2021-01-03 13:20:16 -0600

    https://headsupvideo.atlassian.net/browse/HEADSUP-35

--- src/app/comments/comments.service.ts
+++ src/app/comments/comments.service.ts
@@ -17,27 +17,28 @@ export class CommentsService {
     
     getComments(args: any) {
         let roomSid = args.room.RoomSid
-        let query = ref => ref.where('RoomSid', '==', roomSid).orderBy('date_ms', 'asc')
-        if(args.limit) query = ref => ref.where('RoomSid', '==', roomSid).orderBy('date_ms', 'asc').limit(args.limit)
-        return this.afs.collection('comment', query).snapshotChanges()
+        // let query = ref => ref.where('RoomSid', '==', roomSid).orderBy('date_ms', 'asc')
+        // if(args.limit) query = ref => ref.where('RoomSid', '==', roomSid).orderBy('date_ms', 'asc').limit(args.limit)
+        let query = ref => ref.orderBy('date_ms', 'asc')
+        return this.afs.collection('room').doc(roomSid).collection('comments', query).snapshotChanges()
 
         // ex from logs:  ref = ref.orderBy('date_ms', this.reverse ? 'desc' : 'asc').startAt(this.dates.date2).endAt(this.dates.date1);
     }
 
 
     /*needed?  async*/ create(comment: Comment) {
-        /*await*/ this.afs.collection('comment').doc(comment.commentId).set(comment)
+        /*await*/ this.afs.collection('room').doc(comment.RoomSid).collection('comments').doc(comment.commentId).set(comment)
     }
 
 
     update(comment: Comment) {
         console.log('update(): comment = ', comment)
-        this.afs.collection('comment').doc(comment.commentId).update(comment)
+        this.afs.collection('room').doc(comment.RoomSid).collection('comments').doc(comment.commentId).update(comment)
     }
 
 
     delete(comment: Comment) {
-        this.afs.collection('comment').doc(comment.commentId).delete()
+        this.afs.collection('room').doc(comment.RoomSid).collection('comments').doc(comment.commentId).delete()
     }
 
 
