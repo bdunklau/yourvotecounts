ec60a307d68df7434d90d9f3c636f3a021b6760b
Brent Dunklau <brentdunklau@csitx.com>
2021-01-10 19:50:13 -0600

    have to make sure you're querying the config/settings document in the browser, not the server.  If you don't check for the browser platform, then this query will hang on the server with the error:  Error enabling offline persistence

--- src/app/token/token.component.ts
+++ src/app/token/token.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import * as firebase from 'firebase/app';
 import { UserService } from '../user/user.service';
 import { LogService } from '../log/log.service'
@@ -16,13 +17,22 @@ export class TokenComponent implements OnInit {
   otherStuff: string;
   submitted = false;
   settings: Settings
+  firebase_functions_host = "us-central1-yourvotecounts-dev.cloudfunctions.net"
 
   constructor(private userService: UserService,
+              @Inject(PLATFORM_ID) private platformId,
               private settingsService: SettingsService,
               private log: LogService) { }
 
   async ngOnInit() {
-      this.settings = await this.settingsService.getSettingsDoc()
+
+      /**
+       * Have to wrap in this if block so we don't get the "Error enabling offline persistence" error in server console
+       */
+      if(isPlatformBrowser(this.platformId)) {
+          this.settings = await this.settingsService.getSettingsDoc()
+          this.firebase_functions_host = this.settings.firebase_functions_host
+      }
   }
 
   async onSubmit() {
