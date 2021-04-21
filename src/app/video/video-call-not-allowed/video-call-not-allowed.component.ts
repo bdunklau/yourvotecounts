a3dc41685a0fd5577c11553a1e50fe41ee536c30
Brent Dunklau <brentdunklau@csitx.com>
2021-04-21 09:03:31 -0500

    I changed the instructions when the user doesn't have authority to create a video call.  I put my phone number down instead of TPPF

--- src/app/video/video-call-not-allowed/video-call-not-allowed.component.ts
+++ src/app/video/video-call-not-allowed/video-call-not-allowed.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { LicenseService } from '../../license/license.service';
+import { Licensee } from '../../license/licensee/licensee.model';
+import { isPlatformBrowser } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { SettingsService } from '../../settings/settings.service';
+import { Settings } from '../../settings/settings.model';
 
 
 
@@ -12,9 +19,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoCallNotAllowedComponent implements OnInit {
 
-  constructor() { }
+    licensees: Licensee[]
+    licenseeSubscription: Subscription
+    settings: Settings
+    admin_phone: string
 
-  ngOnInit(): void {
-  }
+    constructor(private licenseService: LicenseService,   
+      private settingsService: SettingsService,       
+      @Inject(PLATFORM_ID) private platformId) { }
+
+    async ngOnInit() {
+        
+        if(isPlatformBrowser(this.platformId)) {
+          
+            this.settings = await this.settingsService.getSettingsDoc()
+            this.admin_phone = this.settings.admin_sms
+            this.licenseeSubscription = this.licenseService.getLicensees().pipe(
+                map(actions => {
+                    return actions.map(a => {
+                        /**
+                         * YOU CAN'T JUST CAST a.payload.doc.data() as Licensee
+                         * because Licensee as a toObj() function that we want
+                         * 
+                         * You have to pass a.payload.doc.data() to the Licensee constructor
+                         */
+                        let licensee = new Licensee(a.payload.doc.data())
+                        return licensee;
+                    });
+                })
+            )
+            .subscribe(objs => {
+                this.licensees = objs
+            });
+
+        }
+    }
 
 }
