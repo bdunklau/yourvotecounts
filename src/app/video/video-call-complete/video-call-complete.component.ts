79e1621729cc417ecb679021548cf6f810e7185e
Brent Dunklau <brentdunklau@csitx.com>
2021-05-04 10:48:52 -0500

    adding stuff from tempa into tempb2 little by little

--- src/app/video/video-call-complete/video-call-complete.component.ts
+++ src/app/video/video-call-complete/video-call-complete.component.ts
@@ -45,6 +45,33 @@ export class VideoCallCompleteComponent implements OnInit {
     }
     
 
+    // async compose_ORIGINAL() {
+    //     this.compositionInProgress = true
+    //     this.publishButtonText = "Workin' on it!..."
+    //     const httpOptions = {
+    //         headers: new HttpHeaders({
+    //           'Content-Type':  'application/json',
+    //           'Authorization': 'my-auth-token',
+    //           'Access-Control-Allow-Origin': '*'
+    //         }),
+    //         //params: new HttpParams().set('uid', uid)
+    //     };
+
+    //     // do a POST here not a GET
+    //     // and post the whole room document.  No need to query for it in the firebase function
+
+    //     let participantCount = this.roomObj.guests ? this.roomObj.guests.length + 1 : 1
+
+    //     // NOTE: ${this.settingsDoc.website_domain_name} will be the ngrok host if running locally.  See invitation.service.ts:ngrok field
+    //     let composeUrl = `https://${this.settingsDoc.firebase_functions_host}/compose?RoomSid=${this.roomObj.RoomSid}&firebase_functions_host=${this.settingsDoc.firebase_functions_host}&room_name=${this.roomObj.invitationId}&website_domain_name=${this.settingsDoc.website_domain_name}&cloud_host=${this.settingsDoc.cloud_host}&participantCount=${participantCount}`
+    //     this.compositionSubscription = this.http.get(composeUrl, httpOptions).subscribe(async (data: any) => {
+    //           console.log('data = ', data) 
+    //           this.router.navigate(['/video/producing'/*, this.roomObj.RoomSid*/])   
+    //     });
+
+    // }
+
+    
     async compose() {
         this.compositionInProgress = true
         this.publishButtonText = "Workin' on it!..."
@@ -64,6 +91,20 @@ export class VideoCallCompleteComponent implements OnInit {
 
         // NOTE: ${this.settingsDoc.website_domain_name} will be the ngrok host if running locally.  See invitation.service.ts:ngrok field
         let composeUrl = `https://${this.settingsDoc.firebase_functions_host}/compose?RoomSid=${this.roomObj.RoomSid}&firebase_functions_host=${this.settingsDoc.firebase_functions_host}&room_name=${this.roomObj.invitationId}&website_domain_name=${this.settingsDoc.website_domain_name}&cloud_host=${this.settingsDoc.cloud_host}&participantCount=${participantCount}`
+
+        /**
+         * Not completely finished yet.  Not completely generalized.  We look at the first guest in the array and see if it has
+         * a participantSid.  Generally, there could be more than one guest. But for this ticket, we are only concerned with
+         * cases where there is only 1 guest
+         * 
+         * https://headsupvideo.atlassian.net/browse/HEADSUP-105
+         */
+        if(this.roomObj.guests && this.roomObj.guests.length == 1 && this.roomObj.guests[0].participantSid) {
+            participantCount = 1
+            let participantSid = this.roomObj.guests[0].participantSid
+            composeUrl = `https://${this.settingsDoc.firebase_functions_host}/compose?RoomSid=${this.roomObj.RoomSid}&firebase_functions_host=${this.settingsDoc.firebase_functions_host}&room_name=${this.roomObj.invitationId}&website_domain_name=${this.settingsDoc.website_domain_name}&cloud_host=${this.settingsDoc.cloud_host}&participantCount=${participantCount}&participantSid=${participantSid}`
+        }
+
         this.compositionSubscription = this.http.get(composeUrl, httpOptions).subscribe(async (data: any) => {
               console.log('data = ', data) 
               this.router.navigate(['/video/producing'/*, this.roomObj.RoomSid*/])   
